95b314ca224aedc932cf577aaf3e4eb43fe8485c
Jo-Anne Rivera <riverajoanne231@gmail.com>
2024-03-01 18:44:38 -0500

    Meant to push these changes with the previous one

--- lie-detection/src/App.js
+++ lie-detection/src/App.js
@@ -1,15 +1,14 @@
-// import logo from './logo.svg';
-import './App.css';
-import SignUp from './components/SignUp';
+import React from 'react';
+import Signup from './components/Signup';
 import Login from './components/Login';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
--- lie-detection/src/components/SignUp.js
+++ lie-detection/src/components/SignUp.js
@@ -1,31 +1,58 @@
-// import { useState } from 'react'
-// import axios from 'axios'
+import { useState } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
-const SignUp = () => {
-  // const [count, setCount] = useState(0)
+const Signup = () => {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:3001/signup', { name, email, password })
+      .then(result => {
+        console.log(result)
+        navigate('/login')
+      })
+    .catch(err => console.log(err))
+  }
 
   return (
     <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
       <div className="bg-white p-3 rounded w-25">
-        <h1>Sign Up</h1>
-        <form>
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
-            <label htmlFor="email" className="form-label">Email</label>
-            <input type="text" className="form-control rounded-0" name="email" placeholder='Enter Email Address' autoComplete='off' />
+            <label htmlFor="email">
+              <strong>Name</strong>
+            </label>
+            <input type="text" className="form-control rounded-0" name="email" placeholder='Enter Name' autoComplete='off' onChange={(e) => setName(e.target.value)} />
           </div>
           <div className="mb-3">
-            <label htmlFor="email" className="form-label">Password</label>
-            <input type="password" className="form-control rounded-0" name="password" placeholder='Enter Password' />
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input type="text" className="form-control rounded-0" name="email" placeholder='Enter Email Address' autoComplete='off' onChange={(e) => setEmail(e.target.value)}/>
           </div>
-          <button type="submit" className="btn btn-success w-100 rounded-0">Sign Up</button>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Password</strong>
+            </label>
+            <input type="password" className="form-control rounded-0" name="password" placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Sign Up
+          </button>
           <p>Already have an account?</p>
-          <Link to="/login" className="btn btn-default borderbg-light rounded-0 text-decoration-none rounded-0">Login</Link>
+          <Link to="/login" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+            Login
+          </Link>
         </form>
       </div>
     </div>
   ); 
 }
 
-export default SignUp;
+export default Signup;
