ac64c80e69fb9c00b013ffdeddfc64c7ce32daab
nprkh13011 <55457971+nprkh13011@users.noreply.github.com>
2024-03-20 22:15:13 -0400

    Image is Visible to User

--- lie-detection/src/App.js
+++ lie-detection/src/App.js
@@ -5,14 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-// import Login from './components/Login';
+import LieTest from './components/LieTest';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter >
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/lietest" element={<LieTest />} />
+        {/* <Route path='/logout' element={<Logout />} /> */}
       </Routes>
     </BrowserRouter>
   )
---
+++ lie-detection/src/components/LieTest.js
@@ -0,0 +1,70 @@
+import React, {Component, useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'bootstrap'
+import {Navigate} from 'react-router-dom';
+
+class LieTest extends Component {
+    constructor(props) {
+        super(props)
+
+        this.onClickForward = this.onClickForward.bind(this)
+
+        const img1 = require('./images/image1.png');
+        const img2 = require('./images/image2.png');
+        const img3 = require('./images/image3.png');
+        const img4 = require('./images/image4.png');
+        const img5 = require('./images/image5.png');
+        const img6 = require('./images/image6.png');
+        const img7 = require('./images/image7.png');
+        const img8 = require('./images/image8.png');
+        const img9 = require('./images/image9.png');
+        const img10 = require('./images/image10.png');
+        const img11 = require('./images/image11.png');
+        const img12 = require('./images/image12.png');
+        const img13 = require('./images/image13.png');
+        const img14 = require('./images/image14.png');
+        const img15 = require('./images/image15.png');
+        const img16 = require('./images/image16.png');
+        const img17 = require('./images/image17.png');
+        
+        this.state = {
+            index: 0,
+            imgList: [img1, img2, img3,img4,img5,img6, img7,
+                      img8, img9, img10, img11, img12, img13,
+                      img14, img15, img16, img17],
+            endOfTest: false
+        }
+    }
+    
+    onClickForward() {
+        //if at the end of the test
+        if (this.state.index + 1 === this.state.imgList.length){
+            this.setState({
+                index: this.state.imgList.length,
+                endOfTest: true
+            })
+        } else { // if we don't reach end of the test
+            this.setState({
+                index: this.state.index + 1
+            })
+        }
+    }
+   render() {
+        return (
+            <div>
+                <div className="d-flex justify-content-center align-items-center vw-100 vh-100">
+                <div className="bg-white p-5 rounded border " style = {{width:'50%'}}>
+                    <h1 className="mb-4">Lie Test</h1>
+                    <h2 className='lietest'>Question {this.state.index + 1}</h2>
+                    <img src = {this.state.imgList[this.state.index]}/><br/>
+                  {this.state.endOfTest && <Navigate to="/login"/> } 
+                    <button onClick={this.onClickForward}>Yes</button><br/>
+                    <button onClick={this.onClickForward}>No</button>
+                </div>
+                </div>
+            </div>
+        )
+        }
+
+}
+export default LieTest;
