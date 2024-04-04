aedb742b7aecc68f53e0a884c88e17bac206dbc1
Simerjeet Mudhar <simerkm@gmail.com>
2024-04-04 04:18:44 -0400

    switch from progress bar to pagination

--- lie-detection/src/components/Progress.js
+++ lie-detection/src/components/Progress.js
@@ -1,23 +1,24 @@
 import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {Pagination} from '@mui/material';
 import {ProgressBar} from 'react-bootstrap';
 
 
 const Progress = () => {
   const [progress, setProgress] = useState(0)
-  const handleProgress = (e) => {
-    let num = progress
-    num = num + 6
-    if (num > 100){
-      num = 100
-    }
-    setProgress(num)
-  }
+  // const handleProgress = (e) => {
+  //   let num = progress
+  //   num = num + 6
+  //   if (num > 100){
+  //     num = 100
+  //   }
+  //   setProgress(num)
+  // }
   return (
     <div >
-      <ProgressBar striped animated now={progress} style={{ width: 500 }}/>
-      <button type="submit" onClick={handleProgress} className="btn btn-secondary w-20 mt-5 rounded-10 ">Next</button>
+      <Pagination hidePrevButton count={17} color="primary" variant="outlined" />
+      {/* <ProgressBar striped animated now={progress} style={{ width: 500 }}/>
+      <button type="submit" onClick={handleProgress} className="btn btn-secondary w-20 mt-5 rounded-10 ">Next</button> */}
     </div>
   )
 }
