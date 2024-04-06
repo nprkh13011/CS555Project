96ffc504a88af1603fbf309daf5fbe437ebd3d07
nprkh13011 <55457971+nprkh13011@users.noreply.github.com>
2024-04-06 11:20:36 -0400

    refactoring

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const UserModel = require('./models/Users')
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const app = express()
 app.use(cors())
