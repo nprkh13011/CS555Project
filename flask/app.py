472a46789dfec27c0df8d3070b55267e3e2679a5
KMuldrow <kmuldrow@stevens.edu>
2024-04-09 08:35:29 -0400

    DB functionality for dummyCode
    allows use of data request despite not having real-time eeg data to work from (will use given training data for now and give a pseudo random prediction on response)

--- flask/app.py
+++ flask/app.py
@@ -2,6 +2,11 @@
 from flask import request, jsonify
 from config import app, db
 from models import Results
+import pandas as pd
+import numpy as np
+import pickle
+
+machineModel = pickle.load(open('model.pkl','rb'))
 
 @app.route("/results", methods=["GET"])
 def get_results():
@@ -12,9 +17,24 @@ def get_results():
 
 @app.route("/create_result", methods=["POST"])
 def create_result():
-    true_false = request.json.get("trueFalse")
 
-    if not true_false:
+    #dummy data for demonstration purposes
+    EEG = pd.read_csv('RawWavesCombined.csv')
+    X_EEG = EEG[['EEG.AF3', 'EEG.T7', 'EEG.Pz', 'EEG.T8', 'EEG.AF4']]
+    X_EEG = np.array(X_EEG)
+    number_of_rows = X_EEG.shape[0] 
+    random_indices = np.random.choice(number_of_rows, size=1, replace=False)
+    pred_on_X = X_EEG[random_indices, :]  
+
+    #ideally, we'd want to retrieve some eeg data from a device but for now we'll use the aforementioned dummy data 
+    eeg_af3 = pred_on_X[0][0]
+    eeg_t7 = pred_on_X[0][1]
+    eeg_Pz = pred_on_X[0][2]
+    eeg_T8 = pred_on_X[0][3]
+    eeg_AF4 = pred_on_X[0][4]
+    true_false = machineModel.predict(pred_on_X)[0]
+
+    if not true_false or not eeg_af3 or not eeg_t7 or not eeg_Pz or not eeg_T8 or not eeg_AF4:
         return (jsonify({"message": "details not included"}) , 400,)
     
     new_result = Results(true_false=true_false)
--- flask/models.py
+++ flask/models.py
@@ -2,10 +2,21 @@ from config import db
 
 class Results(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    eeg_af3 = db.Column(db.Float, nullable = False)
+    eeg_t7 = db.Column(db.Float, nullable = False)
+    eeg_Pz = db.Column(db.Float, nullable = False)
+    eeg_T8 = db.Column(db.Float, nullable = False)
+    eeg_AF4 = db.Column(db.Float, nullable = False)
     true_false = db.Column(db.Integer, unique=False, nullable=False)
+    
 
     def to_json(self):
         return{
             "id": self.id,
+            "eeg_af3": self.eeg_af3,
+            "eeg_t7": self.eeg_t7,
+            "eeg_Pz": self.eeg_Pz,
+            "eeg_T8": self.eeg_T8,
+            "eeg_AF4": self.eeg_AF4,
             "trueFalse": self.true_false
         }
