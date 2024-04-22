3dd67c659ed032d911423af19e71d4f43aa1815e
KMuldrow <kmuldrow@stevens.edu>
2024-04-22 12:59:28 -0400

    Iter-x-withResults

--- lie-detection/src/components/Results.js
+++ lie-detection/src/components/Results.js
@@ -3,7 +3,11 @@ import { useNavigate } from 'react-router-dom';
 
 const ResultsPage = () => {
     const navigate = useNavigate()
-    
+
+    var t = Math.floor(Math.random() * 17)
+    var r = [t,17-t]
+
+
     return (
         <div className="d-flex justify-content-center align-items-center vh-100">
             <div className="peach p-3 rounded w-50 h-70 text-center teal" style={{ width: '50%' }}>
@@ -11,10 +15,10 @@ const ResultsPage = () => {
                 <p>We have compiled your results below including which questions you answered truthfully and untruthfully and your professor rankings.</p>
                 <div class="row">
                     <div class="col">
-                        <b>Questions answered truthfully:</b>
+                        <b>Questions answered truthfully: {r[0]}</b>
                     </div>
                     <div class="col">
-                        <b>Questions answered untruthfully:</b>
+                        <b>Questions answered untruthfully: {r[1]}</b>
                     </div>
                 </div>
                 <div className="mt-3 d-flex justify-content-between">
