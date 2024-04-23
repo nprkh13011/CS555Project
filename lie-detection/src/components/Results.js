23f689f8583aa97a1efc574ee02b4c4a0a4f623d
Winnie Yu <winyu9491@gmail.com>
2024-04-23 12:54:14 -0400

    Changed which questions to how many questions

--- lie-detection/src/components/Results.js
+++ lie-detection/src/components/Results.js
@@ -12,7 +12,7 @@ const ResultsPage = () => {
         <div className="d-flex justify-content-center align-items-center vh-100">
             <div className="peach p-3 rounded w-50 h-70 text-center teal" style={{ width: '50%' }}>
                 <h1 className="mb-4">Thank you for using our application!</h1>
-                <p>We have compiled your results below including which questions you answered truthfully and untruthfully and your professor rankings.</p>
+                <p>We have compiled your results below including how many questions you answered truthfully and untruthfully and your professor rankings.</p>
                 <div class="row">
                     <div class="col">
                         <b>Questions answered truthfully: {r[0]}</b>
